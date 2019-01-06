749245224cf12a40fc8881fb2edd53cbb53d64aa
hijazahmed <44543188+hijazahmed@users.noreply.github.com>
2019-01-06 13:07:28 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+
+const String defaultUserName = "Hijaz Ahmed";
+
+void main () => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+return new MaterialApp(
+title: "chat App",
+home: new Chat(),
+);
+  }
+
+}
+
+class Chat extends StatefulWidget {
+  @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> with TickerProviderStateMixin {
+final List<Msg> _message = <Msg>[];
+final TextEditingController _textController = new TextEditingController();
+bool _iswriting = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("Chat App"),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              itemBuilder: (_, int index)=> _message[index],
+              itemCount: _message.length,
+              reverse: true,
+              padding: EdgeInsets.all(6.0),
+
+            ),
+          ),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(
+            child: _buildComposer(),
+            decoration: new BoxDecoration(
+
+            ),
+          ),
+        ],
+      ),
+      
+    );
+  }
+ Widget _buildComposer(){
+   return new IconTheme(
+data: new IconThemeData(color: Theme.of(context).accentColor),
+child: new Container(
+  margin: const EdgeInsets.symmetric(horizontal: 9.0),
+  child: new Row(
+    children: <Widget>[
+      Flexible(
+        child: new TextField(
+          controller: _textController,
+          onChanged: (String txt){
+            setState(() {
+                          _iswriting = txt.length>0;
+                        });
+          },
+          onSubmitted: _submitMsg,
+          decoration: new InputDecoration.collapsed(hintText: "enter the message"),
+
+        ),
+      ),
+      new Container(
+margin: EdgeInsets.symmetric(horizontal: 3.0),
+child: new IconButton(
+  icon: Icon(Icons.message),
+  onPressed: _iswriting
+  ?()=> _submitMsg(_textController.text):null,
+),
+      ),
+    ],
+  ),
+),
+   );
+ }
+ void _submitMsg(String txt){
+   _textController.clear();
+   setState(() {
+        _iswriting=false;
+      });
+      Msg msg = new Msg(txt: txt,animationController: new AnimationController(
+        vsync: this,
+        duration: new Duration(milliseconds: 800)
+      ),  );
+      setState(() {
+              _message.insert(0, msg);
+
+
+            });
+            msg.animationController.forward();  
+ }
+ @override
+ void dispose(){
+   for(Msg msg in _message){
+     msg.animationController.dispose();
+   }
+   super.dispose();
+ }
+}
+  class Msg extends StatelessWidget{
+    Msg({this.txt, this.animationController});
+    final String txt;
+    final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+  return new SizeTransition(
+    sizeFactor: new CurvedAnimation(
+      parent: animationController,
+      curve: Curves.bounceOut
+    ),
+    axisAlignment: 0.0,
+    child: new Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 18.0),
+            child: new CircleAvatar(
+              child: new Text(defaultUserName[0]),
+            ),
+          ),
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(defaultUserName, style: Theme.of(context).textTheme.subhead,),
+                new Container(
+                  margin: EdgeInsets.only(top: 6.0),
+                  child: new Text(txt),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+  }
+
+  }
